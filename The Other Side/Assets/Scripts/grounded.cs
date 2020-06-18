8f555eca1504c7edc45a0e80a00e2005e3117916
ariefrahmanfajri <18523092@students.uii.ac.id>
2020-06-18 22:30:35 +0700

    first commit

---
+++ The Other Side/Assets/Scripts/grounded.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grounded : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void onCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<movement_BOY>().isGrounded = true;
+        }
+
+    }
+
+    private void onCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<movement_BOY>().isGrounded = false;
+        }
+
+    }
+}
---
+++ The Other Side/Assets/Scripts/movement_BOY.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_BOY : MonoBehaviour
+{
+    public int moveSpeed, kekuatanLompat;
+    Rigidbody2D lompat;
+    bool balik;
+    int pindah;
+
+    public bool isGrounded = false;
+    public bool tanah;
+    public LayerMask targetLayer;
+    public Transform deteksiTanah1;
+    public Transform deteksiTanah2;
+    public float jangkauan;
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lompat = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tanah == true)
+        {
+            anim.SetBool("lompat", false);
+            
+        } else
+        {
+            anim.SetBool("lompat", true);
+        }
+     
+        tanah = Physics2D.OverlapCircle(deteksiTanah1.position, jangkauan, targetLayer);
+        tanah = Physics2D.OverlapCircle(deteksiTanah2.position, jangkauan, targetLayer);
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            pindah = -1;
+            anim.SetBool("jalan", true);
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.right * -moveSpeed * Time.deltaTime);
+            pindah = 1;
+            anim.SetBool("jalan", true);
+        }
+        else
+        {
+            anim.SetBool("jalan", false);
+        }
+
+        if ((Input.GetKey(KeyCode.W)) && tanah == true)
+        {
+            //lompat.AddForce(new Vector2(0, kekuatanLompat));
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5f), ForceMode2D.Impulse);
+        }
+
+        if (pindah>0 && !balik)
+        {
+            balikBadan();
+        } else if (pindah<0 && balik)
+        {
+            balikBadan();
+        }
+    }
+
+    void balikBadan()
+    {
+        balik = !balik;
+        Vector3 karakter = transform.localScale;
+        karakter.x *= -1;
+        transform.localScale = karakter;
+    }
+    
+}
