79dd0603ac1f314e95b667ffa0b8408b55a691fd
ariefrahmanfajri <18523092@students.uii.ac.id>
2020-07-04 22:38:20 +0700

    Dialog Text

---
+++ TheOtherSide/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            TriggerDialogue();
+        }
+        Destroy(GameObject.Find("DialogTrigger"));
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
