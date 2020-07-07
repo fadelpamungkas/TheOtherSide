6b1d5c2246f21118a41c48cdda58deedfba4f0a7
ariefrahmanfajri <18523092@students.uii.ac.id>
2020-07-07 23:25:30 +0700

    Nambahin Inventory

---
+++ TheOtherSide/Assets/Scripts/BendaMistis.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BendaMistis : MonoBehaviour
+{
+    // int jmlhBendaMistis;
+    // public Image[] bendaMistis;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     jmlhBendaMistis = 0;
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     for (int i = 0; i < bendaMistis.Length; i++)
+    //     {
+    //         bendaMistis[i].enabled = true;
+    //     }
+    // }
+
+    // public void AmbilBendaMistis(){
+    //     jmlhBendaMistis ++;
+    // }
+
+    public bool[] isFull;
+    public GameObject[] slots;
+}
---
+++ TheOtherSide/Assets/Scripts/BendaMistisTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BendaMistisTrigger : MonoBehaviour
+{
+    BendaMistis bendaMistis;
+    public GameObject benda;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bendaMistis = GameObject.FindGameObjectWithTag("Player").GetComponent<BendaMistis>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        for (int i = 0; i < bendaMistis.slots.Length; i++)
+        {
+            if(bendaMistis.isFull[i] == false){
+                bendaMistis.isFull[i] = true;   
+                Instantiate(benda, bendaMistis.slots[i].transform, false);
+                Destroy(gameObject);          
+                break;
+            }
+        }
+    }
+}
